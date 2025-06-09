60eb58277d6d1463957e4bd3801dd81fe8c9503c
Edwin Lee <leeed2001@gmail.com>
2025-06-08 20:43:13 -0500

    Beginning of unit testing

--- launch/main.py
+++ launch/main.py
@@ -1,5 +1,5 @@
 try:
-    from board_definitions.raspberry_pi_pico_w import GP10, GP11, GP16, GP17, GP18, LED
+    from board_definitions.raspberry_pi_pico_w import GP10, GP11, GP16, GP17, GP18, GP0, LED
 except ImportError:  # pragma: no cover
     # noinspection PyPackageRequirements
     from board import GP10, GP11, GP16, GP17, GP18, GP0, LED
@@ -14,7 +14,9 @@ from adafruit_display_text import label
 from adafruit_display_text.scrolling_label import ScrollingLabel
 from adafruit_st7735r import ST7735R
 
+# noinspection PyPackageRequirements
 from wifi import radio
+# noinspection PyPackageRequirements
 import socketpool
 import ssl
 from os import getenv
@@ -22,8 +24,6 @@ from os import getenv
 import adafruit_requests
 from adafruit_datetime import datetime, timedelta
 from time import sleep
-
-print("System on internal power")
 
 
 class PicoControl:
@@ -41,8 +41,9 @@ class PicoControl:
         self.led = DigitalInOut(LED)
         self.led.direction = Direction.OUTPUT
         self.pool = socketpool.SocketPool(radio)
+        # noinspection PyTypeChecker
         self.requests = adafruit_requests.Session(self.pool, ssl.create_default_context())
-        self.launch: str = ""
+        self.launch: dict = {}
         self.name: str = ""
         self.t0: str = ""
         self.win_open: str = ""
@@ -57,15 +58,19 @@ class PicoControl:
         self.d: str = ""
         self.h: str = ""
         self.mi: str = ""
+        self.dy: str = ""
         self.button = DigitalInOut(GP0)
         self.button.direction = Direction.INPUT
         self.button.pull = Pull.DOWN
         self.manual_setting: bool = False
+        self.utc_delta: int = 0
 
     def led_toggle(self, toggle: bool):
         self.led.value = toggle
 
-    def manage_memory(self, verbose=False):
+    # noinspection PyUnresolvedReferences
+    @staticmethod
+    def manage_memory(verbose=False):
         if verbose:
             old_memory_available = gc.mem_free()
             gc.collect()
@@ -75,13 +80,13 @@ class PicoControl:
 
     def visuals(self, accent: tuple):
         # Add purple background
-        self.splash = Group()
-        self.display.root_group = self.splash
+        splash = Group()
+        self.display.root_group = splash
         color_bitmap = Bitmap(128, 160, 1)
         color_palette = Palette(1)
         color_palette[0] = accent
         bg_sprite = TileGrid(color_bitmap, pixel_shader=color_palette, x=0, y=0)
-        self.splash.append(bg_sprite)
+        splash.append(bg_sprite)
 
         # Create black rectangle
         # This visually makes the purple background look like just a border
@@ -89,35 +94,35 @@ class PicoControl:
         inner_palette = Palette(1)
         inner_palette[0] = 0x000000  # Black
         inner_sprite = TileGrid(inner_bitmap, pixel_shader=inner_palette, x=5, y=35)
-        self.splash.append(inner_sprite)
+        splash.append(inner_sprite)
 
-        self.countdown_text_group = Group(scale=2, x=16, y=18)
+        countdown_text_group = Group(scale=2, x=16, y=18)
         self.countdown_text_area = label.Label(FONT, text="", color=0x000000)
-        self.countdown_text_group.append(self.countdown_text_area)
-        self.splash.append(self.countdown_text_group)
+        countdown_text_group.append(self.countdown_text_area)
+        splash.append(countdown_text_group)
 
         max_chars = 17
 
-        self.main_text_group = Group(scale=1, x=16, y=50)
-        self.main_row_1 = ScrollingLabel(FONT, y=0, animate_time=0.5, max_characters=max_chars, text=f"", color=accent)
-        self.main_row_2 = ScrollingLabel(FONT, y=15, animate_time=0.5, max_characters=max_chars, text=f"Data by rocket",
+        main_text_group = Group(scale=1, x=16, y=50)
+        self.main_row_1 = ScrollingLabel(FONT, y=0, animate_time=0.5, max_characters=max_chars, text="", color=accent)
+        self.main_row_2 = ScrollingLabel(FONT, y=15, animate_time=0.5, max_characters=max_chars, text="Data by rocket",
                                          color=accent)
-        self.main_row_3 = ScrollingLabel(FONT, y=30, animate_time=0.5, max_characters=max_chars, text=f"launch.live",
+        self.main_row_3 = ScrollingLabel(FONT, y=30, animate_time=0.5, max_characters=max_chars, text="launch.live",
                                          color=accent)
         self.main_row_4 = ScrollingLabel(FONT, y=45, animate_time=0.5, max_characters=max_chars,
-                                         text=f"---------------", color=accent)
+                                         text="---------------", color=accent)
         self.main_row_5 = ScrollingLabel(FONT, y=60, animate_time=0.5, max_characters=max_chars,
-                                         text=f"PicoLaunchTimer", color=accent)
-        self.main_row_6 = label.Label(FONT, y=75, text=f"Version 0.3", color=accent)
-        self.main_row_7 = label.Label(FONT, y=90, text=f"Loading...", color=accent)
-        self.main_text_group.append(self.main_row_1)
-        self.main_text_group.append(self.main_row_2)
-        self.main_text_group.append(self.main_row_3)
-        self.main_text_group.append(self.main_row_4)
-        self.main_text_group.append(self.main_row_5)
-        self.main_text_group.append(self.main_row_6)
-        self.main_text_group.append(self.main_row_7)
-        self.splash.append(self.main_text_group)
+                                         text="PicoLaunchTimer", color=accent)
+        self.main_row_6 = label.Label(FONT, y=75, text="Version 0.3", color=accent)
+        self.main_row_7 = label.Label(FONT, y=90, text="Loading...", color=accent)
+        main_text_group.append(self.main_row_1)
+        main_text_group.append(self.main_row_2)
+        main_text_group.append(self.main_row_3)
+        main_text_group.append(self.main_row_4)
+        main_text_group.append(self.main_row_5)
+        main_text_group.append(self.main_row_6)
+        main_text_group.append(self.main_row_7)
+        splash.append(main_text_group)
 
         print(f"Screen rendered with an accent of RGB value {accent}")
 
@@ -128,18 +133,20 @@ class PicoControl:
         self.main_row_4.update()
         self.main_row_5.update()
 
-    def wifi_connect(self):
+    @staticmethod
+    def wifi_connect():
         while True:
             try:
                 radio.connect(getenv("WIFI"), getenv("PASS"))
                 print(f"Connected to the Internet via {getenv("WIFI")}")
-                break
+                return
             except ConnectionError:
+                print("No connection, trying again in 2 seconds")
+                sleep(2)
                 continue
-            print("No connection, trying again in 2 seconds")
-            sleep(2)
 
-    def get_utc_delta(self, country="America", zone="Chicago", st_delta=-6):
+    def get_utc_delta(self, country="America", zone="Chicago", st_delta=-6) -> int:
+        # noinspection HttpUrlsUsage
         time_response = self.requests.get(f"http://timeapi.io/api/time/current/zone?timeZone={country}%2F{zone}")
         time_content = time_response.json()
         time_response.close()
@@ -159,20 +166,23 @@ class PicoControl:
         return total_delta
 
     def get_launch_info(self):
-        self.response = self.requests.get("https://fdo.rocketlaunch.live/json/launches/next/1")
-        self.content = self.response.json()
-        self.response.close()
+        """ Sets the self.launch variable to the next upcoming launch from rocketlaunch.live"""
+        response = self.requests.get("https://fdo.rocketlaunch.live/json/launches/next/1")
+        content = response.json()
+        response.close()
         try:
-            self.launch = self.content["result"][0]
-            self.t0 = self.launch["t0"]
-            self.win_open = self.launch["win_open"]
-            self.name = self.launch["name"]
-            self.vehicle = self.launch["vehicle"]["name"]
-            self.pad = self.launch["pad"]["name"]
-            self.lc = self.launch["pad"]["location"]["name"]
-            self.country = self.launch["pad"]["location"]["country"]
-        except AttributeError as ae:
-            self.launch = "LAUNCH_INFO_ERROR"
+            self.launch = content["result"][0]
+        except (AttributeError, KeyError):
+            self.launch = {
+                "t0": "ERROR t0",
+                "win_open": "ERROR win_open",
+                "name": "ERROR name",
+                "vehicle": {"name": "ERROR vehicle/name"},
+                "pad": {
+                    "name": "ERROR pad/name",
+                    "location": {"name": "ERROR pad/location/name", "country": "ERROR pad/location/country"}
+                }
+            }
 
     def define_auto_vars(self):  # Separate function simply for running during main countdown loop
         self.t0 = self.launch["t0"]
@@ -194,7 +204,8 @@ class PicoControl:
 
     def manual_launch_info(self):
         # Variables in order of display on screen
-        self.t0 = "2026-02-06T19:00Z"  # T-0 time is formatted as "YYYY-MM-DDTHH:MMZ" where T and Z won't change. Input time should be in UTC.
+        # T-0 time is formatted as "YYYY-MM-DDTHH:MMZ" where T and Z won't change. Input time should be in UTC.
+        self.t0 = "2026-02-06T19:00Z"
         self.name = "Milan-Cortina"
         self.vehicle = "Games of the"
         self.pad = "XXV Winter"
@@ -211,22 +222,20 @@ class PicoControl:
 
         if self.manual_setting:
             self.manual_launch_info()
-        elif not self.manual_setting:
-            self.define_auto_vars()
         else:
-            pass
+            self.define_auto_vars()
 
-        self.utc_launch_time = datetime(int(self.y), int(self.m), int(self.dy), int(self.h), int(self.mi))
+        utc_launch_time = datetime(int(self.y), int(self.m), int(self.dy), int(self.h), int(self.mi))
         overall_utc_delta = timedelta(hours=self.utc_delta)
-        self.full_launch_time = self.utc_launch_time + overall_utc_delta
-        self.launch_date = str(self.full_launch_time).split(" ")[0]
+        full_launch_time = utc_launch_time + overall_utc_delta
+        launch_date = str(full_launch_time).split(" ")[0]
 
         num_cycles = int(http_time / display_interval)
 
         for cycle in range(num_cycles):
             if self.button.value:
                 print("Button pressed, acquiring the newest data")
-                self.countdown_text_area.text = f"LOADING"
+                self.countdown_text_area.text = "LOADING"
                 sleep(0.5)  # Protection against accidental button presses
                 if self.manual_setting:
                     self.manual_setting = False
@@ -236,8 +245,9 @@ class PicoControl:
                     return
 
             current_time = datetime.now()
-            countdown = self.full_launch_time - current_time
+            countdown = full_launch_time - current_time
 
+            # noinspection PyUnresolvedReferences
             total_seconds = int(countdown.total_seconds())
             days = total_seconds // 86400
             hours = total_seconds // 3600
@@ -252,7 +262,7 @@ class PicoControl:
             if hours >= 100:
                 countdown_str = f"{days} Days"
             elif total_seconds <= 0:
-                countdown_str = f"00:00"
+                countdown_str = "00:00"
             else:
                 countdown_str = f"{hour_logic}{minutes:02}:{seconds:02}"
 
@@ -263,7 +273,7 @@ class PicoControl:
             self.main_row_3.text = f"{self.pad}"
             self.main_row_4.text = f"{self.lc}"
             self.main_row_5.text = f"{self.country}"
-            self.main_row_6.text = f"{self.launch_date}"
+            self.main_row_6.text = f"{launch_date}"
             self.main_row_7.text = f"Manual: {self.manual_setting}"
 
             self.countdown_text_area.text = f"{countdown_str}"
@@ -284,7 +294,6 @@ class PicoControl:
         self.wifi_connect()
         self.manual_setting = setting
         self.utc_delta = self.get_utc_delta()
-
         while True:
             if self.manual_setting:
                 pass
@@ -295,5 +304,6 @@ class PicoControl:
 
 
 if __name__ == "__main__":
+    print("System on internal power")
     control = PicoControl()
     control.run_loop(setting=True)
--- launch/test_main.py
+++ launch/test_main.py
@@ -1,9 +1,208 @@
+from sys import modules
+from types import ModuleType
 from unittest import TestCase, main
-from launch.main import PicoControl
+from unittest.mock import MagicMock
+
 
 class TestMain(TestCase):
-    def test_a(self):
+    class FakePin:
+        def switch_to_output(self, value): pass
+
+    # *** Here are a few convenience classes that minimally mock the actual implementation
+    class FakeSocket:
+        pass
+    #     def __init__(self, *_):
+    #         pass
+    #         # self.settimeout = lambda _x: None
+    #         # self.sendto = lambda _x, _y: None
+    #
+    #     # noinspection PyMethodMayBeStatic
+    #     def recv_into(self, x):
+    #         x[:] = (
+    #             b'\x1c\x02\x03\xe8\x00\x00\x02Z\x00\x00\n\xf4\xc7f.F\xeb\x9e\x85\x85\x01s;m\x00'
+    #             b'\x00\x00\x00\x00\x00\x00\x00\xeb\x9e\x8cd\xa7z\xf7\r\xeb\x9e\x8cd\xa7\x82w\xf6'
+    #         )
+    #
+    #     # def __enter__(self):
+    #     #     return self
+    #
+    #     # def __exit__(self, exc_type, exc_value, traceback):
+    #     #     pass
+
+    class FakeSocketPool:
+        AF_INET = None
+        SOCK_DGRAM = None
+
+        def __init__(self, *_):
+            self.getaddrinfo = lambda _x, _y: [[0, 0, 0, 0, 0]]
+            self.socket = lambda _x, _y: TestMain.FakeSocket()
+
+    class FakeResponse:
+        def __init__(self, json_response_data):
+            self.json_response_data = json_response_data
+
+        def json(self):
+            return self.json_response_data
+
+        def close(self):
+            pass
+
+    @classmethod
+    def setUpClass(cls):
+        mock_board = ModuleType('board_definitions.raspberry_pi_pico_w')
+        mock_board.GP0 = TestMain.FakePin()
+        mock_board.GP10 = TestMain.FakePin()
+        mock_board.GP11 = TestMain.FakePin()
+        mock_board.GP16 = TestMain.FakePin()
+        mock_board.GP17 = TestMain.FakePin()
+        mock_board.GP18 = TestMain.FakePin()
+        mock_board.LED = TestMain.FakePin()
+        modules['board_definitions.raspberry_pi_pico_w'] = mock_board
+
+        cls.fake_wifi = ModuleType("wifi")
+        cls.fake_wifi.radio = MagicMock(name="radio")
+        modules["wifi"] = cls.fake_wifi
+
+        fake_dio = ModuleType("digitalio")
+        fake_dio.DigitalInOut = MagicMock(return_value=TestMain.FakePin())
+        fake_dio.Direction = MagicMock()
+        fake_dio.Pull = MagicMock()
+        modules["digitalio"] = fake_dio
+
+        fake_displayio = ModuleType("displayio")
+        fake_displayio.release_displays = MagicMock(return_value=TestMain.FakePin())
+        fake_displayio.Group = MagicMock(return_value=TestMain.FakePin())
+        fake_displayio.Bitmap = MagicMock(return_value=TestMain.FakePin())
+        fake_displayio.Palette = MagicMock(return_value=TestMain.FakePin())
+        fake_displayio.TileGrid = MagicMock(return_value=TestMain.FakePin())
+        modules["displayio"] = fake_displayio
+
+        fake_terminalio = ModuleType("terminalio")
+        fake_terminalio.FONT = MagicMock()
+        modules["terminalio"] = fake_terminalio
+
+        fake_fourwire = ModuleType("fourwire")
+        fake_fourwire.FourWire = MagicMock()
+        modules["fourwire"] = fake_fourwire
+
+        fake_7735 = ModuleType("adafruit_st7735r")
+        fake_7735.ST7735R = MagicMock()
+        modules["adafruit_st7735r"] = fake_7735
+
+        fake_socketpool = ModuleType("socketpool")
+        fake_socketpool.SocketPool = MagicMock(return_value=TestMain.FakeSocketPool())
+        modules["socketpool"] = fake_socketpool
+
+        fake_busio = ModuleType("busio")
+        fake_busio.SPI = MagicMock()
+        modules["busio"] = fake_busio
+
+        # fake_conn_manager = ModuleType("adafruit_connection_manager")
+        # fake_conn_manager.get_radio_socketpool = MagicMock(return_value=TestMain.FakeSocketPool())
+        # fake_conn_manager.get_radio_ssl_context = MagicMock(return_value=MagicMock())
+        # modules["adafruit_connection_manager"] = fake_conn_manager
+
+        fake_requests = ModuleType("adafruit_requests")
+        cls.fake_session_instance = MagicMock()
+        cls.fake_session_instance.get = MagicMock(name="get")
+        fake_requests.Session = MagicMock(return_value=cls.fake_session_instance)
+        modules["adafruit_requests"] = fake_requests
+
+        from launch.main import PicoControl
+        cls.control = PicoControl
+
+    def test_get_launch_info(self):
+        p = self.control()
+        # call it once with valid response data
+        self.fake_session_instance.get.return_value = TestMain.FakeResponse(
+            {
+                "result": [
+                    {
+                        "name": "Ax-4",
+                        "provider": {"name": "SpaceX"},
+                        "vehicle": {"name": "Falcon 9"},
+                        "pad": {
+                            "name": "LC-39A",
+                            "location": {
+                                "name": "Kennedy Space Center",
+                                "state": "FL",
+                                "statename": "Florida",
+                                "country": "United States",
+                            }
+                        },
+                        "missions": [{"name": "Ax-4"}],
+                        "win_open": None,
+                        "t0": "2025-06-10T12:22Z",
+                        "win_close": None,
+                    }
+                ]
+            }
+        )
+        p.get_launch_info()
+        self.assertIsInstance(p.launch, dict)
+        self.assertEqual("2025-06-10T12:22Z", p.launch["t0"])
+        # then call it with empty response data
+        self.fake_session_instance.get.return_value = TestMain.FakeResponse({})
+        p.get_launch_info()
+        self.assertEqual("ERROR t0", p.launch["t0"])
+
+    def test_define_auto_vars(self):
+        p = self.control()
+        p.launch = {
+            "name": "Ax-4",
+            "provider": {"name": "SpaceX"},
+            "vehicle": {"name": "Falcon 9"},
+            "pad": {
+                "name": "LC-39A",
+                "location": {
+                    "name": "Kennedy Space Center",
+                    "state": "FL",
+                    "statename": "Florida",
+                    "country": "United States",
+                }
+            },
+            "missions": [{"name": "Ax-4"}],
+            "win_open": "2025-06-10T12:00Z",
+            "t0": "2025-06-10T12:22Z",
+            "win_close": "2025-06-10T13:00Z",
+        }
+        p.define_auto_vars()
+        self.assertEqual("2025-06-10T12:22Z", p.t0)
+        p.launch["t0"] = None
+        p.define_auto_vars()
+        self.assertEqual("2025-06-10T12:00Z", p.t0)
+
+    def test_manual_launch_info(self):
+        p = self.control()
+        p.manual_launch_info()
+        self.assertNotEqual("", p.t0)
+
+    def test_manage_memory(self):
+        # test it once with verbose and one not
+        #p = self.control()
         pass
 
-if __name__ == "__main__":
+    def test_countdown_loop(self):
+        pass
+
+    def test_run_loop(self):
+        pass  # probably want to add an argument to run it once without the countdown loop
+
+    def test_get_utc_delta(self):
+        pass
+
+    def test_wifi_connected(self):
+        pass
+
+    def test_update_scrolls(self):
+        pass
+
+    def test_visuals(self):
+        pass
+
+    def test_led_toggle(self):
+        pass
+
+
+if __name__ == "__main__":  # pragma: no cover
     main()
